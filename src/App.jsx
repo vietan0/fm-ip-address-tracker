e49825672b20ad75fa9b5bacea518c106c423489
Việt An <vietan00892b@gmail.com>
2023-04-21 14:21:15 +0700

    Initialized project, included FM files, basic CSS

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+export default function App() {
+  return (
+    <>
+      <h1>IP Address Tracker</h1>
+      <div className="map"></div>
+      <div className="attribution">
+        Challenge by{' '}
+        <a
+          href="https://www.frontendmentor.io?ref=challenge"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Frontend Mentor
+        </a>
+        . Coded by <a href="https://github.com/vietan0">Việt An</a>.
+      </div>
+    </>
+  );
+}
